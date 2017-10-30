23fa7d538f0ac6ec94c4e089734e2be1427fa7b6
Denis K <dk.pochtamp@gmail.com>
2017-10-31 01:25:12 +0800

    Update 500sales.inmrkt.ml

--- 500sales.inmrkt.ml
+++ 500sales.inmrkt.ml
@@ -1,46 +1,41 @@
 server {
 
-    listen       80; # listen for IPv4
-    # listen       [::]:80 ipv6only=on; # listen for IPv6
+    listen 80; # listen for IPv4
 
-    server_name  500sales.inmrkt.ml;
+    # listen [::]:80 ipv6only=on; # listen for IPv6
 
-    set $root_path  /var/www/500sales.inmrkt.ml/html;
+    server_name 500sales.inmrkt.ml;
+    set $root_path /var/www/500sales.inmrkt.ml/html;
+    root $root_path;
 
-    root         $root_path;
+    # access_log off; error_log /dev/null crit;
 
-    #access_log   off;
-    #error_log    /dev/null crit;
-    charset      utf-8;
-    client_max_body_size  100M;
+    charset utf-8;
+    client_max_body_size 100M;
 
     location / {
-        root  $root_path/backend/web;
-        try_files  $uri /backend/web/index.php$is_args$args;
+        root $root_path/backend/web;
+        try_files $uri /backend/web/index.php$is_args$args;
     }
 
     location ~ \.php$ {
+        include /etc/nginx/fastcgi_params;
 
-	include  /etc/nginx/fastcgi_params;       
-       	
-	# check the www.conf file to see if PHP-FPM is listening on a socket or port
+        # check the www.conf file to see if PHP-FPM is listening on a socket or port fastcgi_pass 127.0.0.1:9000; fastcgi_split_path_info ^(.+\.php)(/.+)$;
 
-	# fastcgi_pass  127.0.0.1:9000;
-	# fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        
-	fastcgi_pass  unix:/var/run/php/php7.1-fpm.sock;
-        fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        
-	try_files  $uri /backend/web$uri =404;
+        fastcgi_pass unix:/run/php/php7.1-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
 
+        try_files $uri /backend/web$uri =404;
     }
 
     # avoid processing of calls to non-existing static files by Yii (uncomment if necessary)
 
-    location ~ \.(css|js|jpg|jpeg|png|gif|bmp|ico|mov|swf|pdf|zip|rar)$ {
-    #    access_log  off;
-    #    log_not_found  off;
-       try_files  $uri /backend/web$uri =404;
+    location ~ \.(css|less|ttf|otf|woff|woff2|js|jpg|jpeg|png|gif|bmp|ico|mov|swf|pdf|zip|rar)$ {
+
+    #   access_log off; log_not_found off;
+
+        try_files $uri =404;
     }
 
     location ~* \.(htaccess|htpasswd|svn|git) {
@@ -48,39 +43,33 @@ server {
     }
 
     location /front {
-        
-	alias  $root_path/frontend/web;
+
+        alias $root_path/frontend/web;
 
         # redirect to the page without a trailing slash (uncomment if necessary)
-        
-	location = /front/ {
-           return  301 /front;
+
+        location = /front/ {
+           return 301 /front;
         }
 
         location ~ ^/front/(.+\.php)$ {
-        
-	    include  /etc/nginx/fastcgi_params;
-            
-	    # check the www.conf file to see if PHP-FPM is listening on a socket or port
 
-	    # fastcgi_pass  127.0.0.1:9000;
-	    # fastcgi_split_path_info ^(.+\.php)(/.+)$;
-            
-	    fastcgi_pass  unix:/var/run/php/php7.1-fpm.sock;
-            fastcgi_param  SCRIPT_FILENAME $document_root/$1;
+            include /etc/nginx/fastcgi_params;
 
-            try_files  $uri /frontend/web/$1$is_args$args;
-        
-	}
+            # check the www.conf file to see if PHP-FPM is listening on a socket or port fastcgi_pass 127.0.0.1:9000; fastcgi_split_path_info ^(.+\.php)(/.+)$;
+
+            fastcgi_pass unix:/run/php/php7.1-fpm.sock;
+            fastcgi_param SCRIPT_FILENAME $document_root/$1;
+            try_files $uri /frontend/web/$1$is_args$args;
+
+        }
 
         # avoid processing of calls to non-existing static files by Yii (uncomment if necessary)
-	
-	location ~ ^/front/(.+\.(css|js|jpg|jpeg|png|gif|bmp|ico|mov|swf|pdf|zip|rar))$ {
-		try_files  $uri /frontend/web/$1$is_args$args;
-	}
 
-        try_files  $uri /frontend/web/index.php$is_args$args;
+        location ~ ^/front/(.+\.(css|js|jpg|jpeg|png|gif|bmp|ico|mov|swf|pdf|zip|rar))$ {
+                try_files $uri /frontend/web/$1$is_args$args;
+        }
 
+        try_files $uri /frontend/web/index.php$is_args$args;
     }
-
 }
